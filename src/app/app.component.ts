f8046f1f13b22efcc3bb422c083fa6849196ee08
TechSammy <mrsam03102002@gmail.com>
2024-02-22 15:29:49 +0530

    completed routing between the all,completed,important components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterLinkActive, RouterModule, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent, SidebarComponent],
+  imports: [CommonModule,RouterOutlet,HeaderComponent,SidebarComponent,RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
